51c311c249f471c75d365d26216e8767fb8177c6
Starlord-Code <Harsha.Vardhan.letscode@users.noreply.github.com>
2021-05-10 15:25:01 +0530

    hompage_changes

---
+++ Home Pages/Event Coordinator homepage.php
@@ -0,0 +1,68 @@
+<?php 
+session_start();
+include('../config.php');
+include('includes_EC/header.php');
+
+ 
+
+?>
+
+<head>
+<title>Dashboard</title>
+</head> 
+<section class="main-banner">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="banner-content">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="banner-caption">
+                                                <h4>Home Page of <em>Event Coordinator</em></h4>
+
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section class="services1">
+                    <div class="container-fluid">
+                        <div class="row1">
+                            <div class="col-md-12">
+                                <div class="banner-content">
+                                    <div class="row1">
+                                        <div class="col-md-12">
+                                            <div class="banner-caption">
+
+                                                <span>Choose options from the  left panel to view or update marks.</span>
+                                                <p>For any queries or suggestions contact <strong>cb.en.u4cse18124@cb.students.amrita.edu</strong>.</p>
+                                                <div class="primary-button">
+                                                    <a href="#">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+
+
+
+
+
+<?php
+    include('includes_EC/navbar.php');
+    include('includes_EC/scripts.php');
+    include('includes_EC/footer.php');
+   
+?>   
---
+++ Home Pages/includes_EC/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+    <title>Event Coordinator Homepage</title>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+</head>
+
+<body class="is-preload">
+
+    <!-- Wrapper -->
+    <div id="wrapper">
+
+        <!-- Main -->
+        <div id="main">
+            <div class="inner">
+
+                <!-- Header -->
+                <header id="header">
+                    <div class="logo">
+                        <a href="#">Grace Mark Calculator</a>
+                    </div>
+                </header>
