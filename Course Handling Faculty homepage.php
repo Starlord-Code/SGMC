f1c3a1a88665a0647b9982525bad99978955ae07
Karancse <83109484+Karancse@users.noreply.github.com>
2021-06-11 00:13:40 +0530

    CM,CHFeditprofile
    Course Handling Faculty and Course Mentor can edit their profiles.

---
+++ Course Handling Faculty homepage.php
@@ -0,0 +1,66 @@
+<?php 
+session_start();
+include('../config.php');
+include('includes_CHF/header.php');
+
+ 
+
+?>
+<head>
+<title>Dashboard</title> 
+</head>
+
+                <!-- Banner -->
+                <section class="main-banner">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="banner-content">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="banner-caption">
+                                                <h4>Home Page of <em>Course Handling Faculty</em></h4>
+                                                
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section class="services1">
+                    <div class="container-fluid">
+                        <div class="row1">
+                            <div class="col-md-12">
+                                <div class="banner-content">
+                                    <div class="row1">
+                                        <div class="col-md-12">
+                                            <div class="banner-caption">
+                                               
+                                                <span>Choose options from the  left panel to view or update marks.</span>
+                                                <p>For any queries or suggestions contact <strong></strong>.</p>
+                                                <div class="primary-button">
+                                                    <a href="#">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+
+            </div>
+        </div>
+
+
+<?php
+    include('includes_CHF/navbar.php');
+    include('includes_CHF/scripts.php');
+    include('includes_CHF/footer.php');
+   ?>
